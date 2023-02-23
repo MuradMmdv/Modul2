a035d34ceec4c5f2be514b56d9a3d4f25f4cce2f
MuradMmdv <mamedovmurad11@gmail.com>
2023-02-23 17:59:16 +0400

    js is ready

--- script.js
+++ script.js
@@ -2,16 +2,21 @@
 const clickOval = document.querySelector('.oval');
 let list= document.querySelector('ol');
 let input= document.querySelector('input');
-let deleteIcon = document.querySelector(".cancel");
+let deleteIcon = document.querySelector(".secondCancel");
 const filterButton = document.querySelector(".filter");
+let deleteInput = document.querySelector('.cancel')
 
 
+deleteInput.addEventListener("click", () => {
+  input.value=''
+})
 
 
 clickOval.addEventListener("click", () => {
     list.style.border='1px solid #C4C4C4';
     list.style.borderRadius= '10px';
     list.style.marginTop= '20px';
+    deleteIcon.style.display='block'
 
     if(input.value==''){
         alert('Please enter step')
@@ -32,15 +37,22 @@ clickOval.addEventListener("click", () => {
   deleteButton.appendChild(deleteIcon.cloneNode(true));
   deleteButton.addEventListener("click", () => {
     list.removeChild(listItem);
+    
   });
   listItem.appendChild(deleteButton);
 
   list.appendChild(listItem);
-}
-});
-function clearInput() {
-    document.getElementsByTagName("input").value = ""; // Giriş alanının içeriğini boşalt
-  }
-  
+  listItem.style.transitionDuration='1s'
 
+}
+input.value=''
+
+});
+
+filterButton.addEventListener("click", () => {
+  const listItems = document.querySelectorAll(".list-item");
+  const sortedListItems = Array.from(listItems).sort((a, b) => a.textContent.localeCompare(b.textContent));
+  list.innerHTML = "";
+  sortedListItems.forEach((item) => list.appendChild(item));
+});
 
